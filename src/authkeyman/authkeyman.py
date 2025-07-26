ae3f25d1ebef7cff6b71ab9508dfb090ed5a033f
Tactical Laptop Bag <tacticallaptopbag@protonmail.com>
2025-07-26 05:31:33 -0400

    Move classes to their own files

--- src/authkeyman/authkeyman.py
+++ src/authkeyman/authkeyman.py
@@ -3,70 +3,8 @@ import os
 from argparse import ArgumentParser
 from typing import Optional
 
-
-class AuthorizedKey:
-    def __init__(self, key_type: str, key: str, comment: str):
-        self.key_type = key_type
-        self.key = key
-        self.comment = comment
-
-    @classmethod
-    def from_line(cls, line: str) -> Optional["AuthorizedKey"]:
-        line_split = line.split()
-        if len(line_split) < 3:
-            return None
-        
-        return cls(line_split[0], line_split[1], " ".join(line_split[2:]))
-
-    def __str__(self) -> str:
-        return f"{self.key_type} {self.key} {self.comment}"
-
-        
-class AuthorizedKeysFile:
-    def __init__(self, path: Path, user: Optional[str] = None):
-        self.path = path
-        self.keys: list[AuthorizedKey] = []
-        self.user = user
-
-        try:
-            path.parent.mkdir(mode=0o775, exist_ok=True)
-            path.touch(mode=0o600, exist_ok=True)
-
-            with open(path, "r") as file:
-                lines = file.readlines()
-                for line in lines:
-                    key = AuthorizedKey.from_line(line)
-                    if key is None:
-                        print(f"Failed to parse key in {path}:")
-                        print(line)
-                        exit(1)
-                    self.keys.append(key)
-        except PermissionError:
-            print("Insufficient permissions!")
-            if user is None:
-                print(f"Try checking the permissions on your home directory.")
-            else:
-                print(f"You must run as sudo when adding keys to other users.")
-            exit(1)
-
-    def get_key_from_comment(self, comment: str) -> Optional[AuthorizedKey]:
-        comment = comment.lower()
-        for key in self.keys:
-            if key.comment.lower() == comment:
-                return key
-        return None
-
-    def save(self):
-        with open(self.path, "w") as file:
-            for key in self.keys:
-                file.write(f"{key}\n")
-        return None
-
-    def __str__(self) -> str:
-        string = ""
-        for key in self.keys:
-            string += str(key) + "\n"
-        return string
+from authkeyman.authorized_key import AuthorizedKey
+from authkeyman.authorized_keys_file import AuthorizedKeysFile
 
 
 def _get_auth_keys_files(users: Optional[str]) -> list[AuthorizedKeysFile]:
